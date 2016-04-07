61635986499d202c4783bcd5dfdf4f6749b1b3df
Thomas Dieumegard <tdieumeg@student.42.fr>
2016-04-07 19:33:37 +0200

    testing args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::env;
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn check_args() -> (usize, Result<Vec<String>, Vec<String>>) {
+    let argv: Vec<_> = env::args().collect();
+    let argc = argv.len();
+
+    (argc, match argc {
+        1 => Err(argv),
+        _ => Ok(argv),
+    })
+}
+
+fn read_files(argc: usize, argv: Vec<String>) -> Vec<String> {
+    let mut buffer = String::new();
+    let mut result: Vec<String>;
+
+    for i in 0..argc {
+        let mut f = match File::open(argv[i]) {
+                        Ok(n) => n,
+                        _ => panic!("Could not open file {}", argv[i]),
+                    };
+        match f.read_to_string(&mut buffer) {
+            Ok(n) => n,
+            _ => panic!("Could not read file {}", argv[i]),
+        };
+        result.push(buffer);
+    }
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (argc, argv) = match check_args() {
+        (n, Ok(m)) => (n, m),
+        (_, Err(m)) => { println!("Usage:\t{} puzzle_file [extra_puzzles]", m[0]); return },
+    };
+    let result = read_files(argc, argv);
+
+    for i in 0..argc {
+        println!("File {} => {}", i, result[i]);
+    }
 }
