70244504618b35fb953381fea5548ea22314dc01
afaucher17 <aymeric.faucher@gmail.com>
2016-04-08 19:47:39 +0200

    main.rs : only one file, cleaner code

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,8 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::path::Path;
 
-fn check_args() -> (usize, Result<Vec<String>, Vec<String>>) {
-    let argv: Vec<_> = env::args().collect();
-    let argc = argv.len();
-
-    (argc, match argc {
-        1 => Err(argv),
-        _ => Ok(argv),
-    })
-}
-
-fn read_files(argc: usize, argv: Vec<String>) -> Vec<String> {
-    let mut result = Vec::new();
-    for i in 1..argc {
-        let path = Path::new(&argv[i]);
+fn read_files(filename: &String) -> String {
+        let path = Path::new(filename);
         let display = path.display();
         let mut file = match File::open(&path) {
             Err(why) => panic!("Could not open {}: {}", display, Error::description(&why)),
@@ -26,20 +14,16 @@ fn read_files(argc: usize, argv: Vec<String>) -> Vec<String> {
         let mut buffer = String::new();
         match file.read_to_string(&mut buffer) {
             Err(why) => panic!("Could not read {}: {}", display, Error::description(&why)),
-            Ok(_) => result.push(buffer),
-        };
-    };
-    result
+            Ok(_) => buffer,
+        }
 }
 
 fn main() {
-    let (argc, argv) = match check_args() {
-        (_, Err(m)) => { println!("Usage:\t{} puzzle_file [extra_puzzles]", m[0]); return },
-        (n, Ok(m)) => (n, m),
+    let argv : Vec<String> = env::args().collect();
+    match argv.len() {
+        1 => { println!("Usage:\t{} puzzle_file [extra_puzzles]", &argv[0]); return },
+        _ => (),
     };
-    let result = read_files(argc, argv);
-
-    for i in 0..argc - 1 {
-        println!("File {} => {}", i, result[i]);
-    }
+    let result = read_files(&argv[1]);
+    print!("File 1 => {}", result);
 }
