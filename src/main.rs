2e4a48949dafff2d1f096906d5e7733733ca1b08
Thomas Dieumegard <tdieumeg@student.42.fr>
2016-04-08 15:12:06 +0200

    main pushed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::env;
 use std::io;
 use std::io::prelude::*;
@@ -14,27 +15,27 @@ fn check_args() -> (usize, Result<Vec<String>, Vec<String>>) {
 }
 
 fn read_files(argc: usize, argv: Vec<String>) -> Vec<String> {
-    let mut buffer = String::new();
-    let mut result: Vec<String>;
+    let mut result = Vec::new();
 
     for i in 0..argc {
-        let mut f = match File::open(argv[i]) {
-                        Ok(n) => n,
-                        _ => panic!("Could not open file {}", argv[i]),
-                    };
-        match f.read_to_string(&mut buffer) {
-            Ok(n) => n,
-            _ => panic!("Could not read file {}", argv[i]),
+        let mut file = match File::open(argv[i]) {
+            Err(why) => panic!("Could not open {}: {}", argv[i], Error::description(&why)),
+            Ok(file) => file,
         };
-        result.push(buffer);
-    }
+
+        let mut buffer = String::new();
+        match file.read_to_string(&mut buffer) {
+            Err(why) => panic!("Could not read {}: {}", argv[i], Error::description(&why)),
+            Ok(_) => result.push(buffer),
+        };
+    };
     result
 }
 
 fn main() {
     let (argc, argv) = match check_args() {
-        (n, Ok(m)) => (n, m),
         (_, Err(m)) => { println!("Usage:\t{} puzzle_file [extra_puzzles]", m[0]); return },
+        (n, Ok(m)) => (n, m),
     };
     let result = read_files(argc, argv);
 
