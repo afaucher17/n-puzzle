beafd2ea2c582c61c9bb7a71a4efbd059074987a
afaucher17 <aymeric.faucher@gmail.com>
2016-04-11 21:07:27 +0200

    main.rs mod.rs : array regex

--- src/main.rs
+++ src/main.rs
@@ -27,9 +27,6 @@ fn main() {
         _ => (),
     };
     let result: String = read_files(&argv[1]);
-    let vec: Vec<_> = result.split("\n").map(|s| s.to_string()).collect();
-    for s in parser::remove_comments(vec)
-    {
-        println!("{}", s);
-    }
+    let vec: Vec<String> = result.split("\n").map(|s| s.to_string()).collect();
+    parser::to_array(parser::remove_comments(vec));
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -5,3 +5,17 @@ pub fn remove_comments(lines: Vec<String>) -> Vec<String>
     let rgx = regex::Regex::new(r"(#.*)$|(^#.*$)").unwrap();
     lines.iter().map(|x| rgx.replace(x, "")).filter(|x| *x != "").collect()
 }
+
+pub fn to_array(lines: Vec<String>) -> u32
+{
+    let size: u32 = match lines[0].parse::<u32>() {
+        Ok(n) => match n {
+            0...2 => panic!("The array size must be equal or greater than 3."),
+            _ => n,
+        },
+        Err(_) => panic!("The array size is invalid."),
+    };
+    let s = (1..size*size).map(|i| if i % size == 0 { r"( +\d)\\n" } else { r"( +\d)"}).collect::<String>();
+    println!("{}", s);
+    size
+}
