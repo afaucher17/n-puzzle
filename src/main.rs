1dfb53eba958f8c559b6e79dd04be70f10ea78d2
Thomas Dieumegard <tdieumeg@student.42.fr>
2016-04-15 15:09:01 +0200

    gg=G

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,17 @@ use std::fs::File;
 use std::path::Path;
 
 fn read_files(filename: &String) -> String {
-        let path = Path::new(filename);
-        let display = path.display();
-        let mut file = match File::open(&path) {
-            Err(why) => panic!("Could not open {}: {}", display, Error::description(&why)),
-            Ok(file) => file,
-        };
-        let mut buffer = String::new();
-        match file.read_to_string(&mut buffer) {
-            Err(why) => panic!("Could not read {}: {}", display, Error::description(&why)),
-            Ok(_) => buffer,
-        }
+    let path = Path::new(filename);
+    let display = path.display();
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("Could not open {}: {}", display, Error::description(&why)),
+        Ok(file) => file,
+    };
+    let mut buffer = String::new();
+    match file.read_to_string(&mut buffer) {
+        Err(why) => panic!("Could not read {}: {}", display, Error::description(&why)),
+        Ok(_) => buffer,
+    }
 }
 
 fn main() {
